ab201387d744e8abc3016d55cd0c33da2f8691c1
Stevelet <stijn.swart@gmail.com>
2022-05-03 15:47:25 +0200

    Complete the 3 files

--- class/client.js
+++ class/client.js
@@ -24,20 +24,25 @@ app.set('views', 'files/client');
 // client information
 
 var client = {
-	
+	"client_id": "oauth-client-1",
+	"client_secret": "oauth-client-secret-1",
+	"redirect_uris": ["http://localhost:9000/callback"],
+	"scope": "openid profile email phone address"
 };
 
 // authorization server information
 var authServer = {
-
+	authorizationEndpoint: 'http://localhost:9001/authorize',
+	tokenEndpoint: 'http://localhost:9001/token',
+	userInfoEndpoint: 'http://localhost:9002/userinfo'
 };
 
 var rsaKey = {
-  "alg": "RS256",
-  "e": "AQAB",
-  "n": "p8eP5gL1H_H9UNzCuQS-vNRVz3NWxZTHYk1tG9VpkfFjWNKG3MFTNZJ1l5g_COMm2_2i_YhQNH8MJ_nQ4exKMXrWJB4tyVZohovUxfw-eLgu1XQ8oYcVYW8ym6Um-BkqwwWL6CXZ70X81YyIMrnsGTyTV6M8gBPun8g2L8KbDbXR1lDfOOWiZ2ss1CRLrmNM-GRp3Gj-ECG7_3Nx9n_s5to2ZtwJ1GS1maGjrSZ9GRAYLrHhndrL_8ie_9DS2T-ML7QNQtNkg2RvLv4f0dpjRYI23djxVtAylYK4oiT_uEMgSkc4dxwKwGuBxSO0g9JOobgfy0--FUHHYtRi0dOFZw",
-  "kty": "RSA",
-  "kid": "authserver"
+	"alg": "RS256",
+	"e": "AQAB",
+	"n": "p8eP5gL1H_H9UNzCuQS-vNRVz3NWxZTHYk1tG9VpkfFjWNKG3MFTNZJ1l5g_COMm2_2i_YhQNH8MJ_nQ4exKMXrWJB4tyVZohovUxfw-eLgu1XQ8oYcVYW8ym6Um-BkqwwWL6CXZ70X81YyIMrnsGTyTV6M8gBPun8g2L8KbDbXR1lDfOOWiZ2ss1CRLrmNM-GRp3Gj-ECG7_3Nx9n_s5to2ZtwJ1GS1maGjrSZ9GRAYLrHhndrL_8ie_9DS2T-ML7QNQtNkg2RvLv4f0dpjRYI23djxVtAylYK4oiT_uEMgSkc4dxwKwGuBxSO0g9JOobgfy0--FUHHYtRi0dOFZw",
+	"kty": "RSA",
+	"kid": "authserver"
 };
 
 var protectedResource = 'http://localhost:9002/resource';
@@ -55,19 +60,181 @@ app.get('/', function (req, res) {
 });
 
 app.get('/authorize', function(req, res){
-	res.end();
+
+	access_token = null;
+	refresh_token = null;
+	scope = null;
+	state = randomstring.generate();
+
+	var authorizeUrl = buildUrl(authServer.authorizationEndpoint, {
+		response_type: 'code',
+		scope: client.scope,
+		client_id: client.client_id,
+		redirect_uri: client.redirect_uris[0],
+		state: state
+	});
+
+	console.log("redirect", authorizeUrl);
+	res.redirect(authorizeUrl);
 });
 
 app.get("/callback", function(req, res){
-	res.end();
+
+	if (req.query.error) {
+		// it's an error response, act accordingly
+		res.render('error', {error: req.query.error});
+		return;
+	}
+
+	var resState = req.query.state;
+	if (resState == state) {
+		console.log('State value matches: expected %s got %s', state, resState);
+	} else {
+		console.log('State DOES NOT MATCH: expected %s got %s', state, resState);
+		res.render('error', {error: 'State value did not match'});
+		return;
+	}
+
+	var code = req.query.code;
+
+	var form_data = qs.stringify({
+		grant_type: 'authorization_code',
+		code: code,
+		redirect_uri: client.redirect_uris[0]
+	});
+	var headers = {
+		'Content-Type': 'application/x-www-form-urlencoded',
+		'Authorization': 'Basic ' + encodeClientCredentials(client.client_id, client.client_secret)
+	};
+
+	var tokRes = request('POST', authServer.tokenEndpoint,
+		{
+			body: form_data,
+			headers: headers
+		}
+	);
+
+	console.log('Requesting access token for code %s',code);
+
+	if (tokRes.statusCode >= 200 && tokRes.statusCode < 300) {
+		var body = JSON.parse(tokRes.getBody());
+
+		access_token = body.access_token;
+		console.log('Got access token: %s', access_token);
+		if (body.refresh_token) {
+			refresh_token = body.refresh_token;
+			console.log('Got refresh token: %s', refresh_token);
+		}
+
+		scope = body.scope;
+		console.log('Got scope: %s', scope);
+
+		if (body.id_token) {
+			userInfo = null;
+			id_token = null;
+
+			console.log('Got ID token: %s', body.id_token);
+
+			// check the id token
+			var pubKey = jose.KEYUTIL.getKey(rsaKey);
+			var signatureValid = jose.jws.JWS.verify(body.id_token, pubKey, [rsaKey.alg]);
+			if (signatureValid) {
+				console.log('Signature validated.');
+				var tokenParts = body.id_token.split('.');
+				var payload = JSON.parse(base64url.decode(tokenParts[1]));
+				console.log('Payload', payload);
+				if (payload.iss == 'http://localhost:9001/') {
+					console.log('issuer OK');
+					if ((Array.isArray(payload.aud) && __.contains(payload.aud, client.client_id)) ||
+						payload.aud == client.client_id) {
+						console.log('Audience OK');
+
+						var now = Math.floor(Date.now() / 1000);
+
+						if (payload.iat <= now) {
+							console.log('issued-at OK');
+							if (payload.exp >= now) {
+								console.log('expiration OK');
+
+								console.log('Token valid!');
+
+								// save just the payload, not the container (which has been validated)
+								id_token = payload;
+
+							}
+						}
+					}
+				}
+			}
+			res.render('userinfo', {userInfo: userInfo, id_token: id_token});
+
+		} else {
+			res.render('index', {access_token: access_token, refresh_token: refresh_token, scope: scope});
+		}
+
+	} else {
+		res.render('error', {error: 'Unable to fetch access token, server response: ' + tokRes.statusCode})
+	}
 });
 
 app.get('/fetch_resource', function(req, res) {
-	res.end();
+
+	if (!access_token) {
+		res.render('error', {error: 'Missing access token.'});
+		return;
+	}
+
+	console.log('Making request with access token %s', access_token);
+
+	var headers = {
+		'Authorization': 'Bearer ' + access_token,
+		'Content-Type': 'application/x-www-form-urlencoded'
+	};
+
+	var resource = request('POST', protectedResource,
+		{headers: headers}
+	);
+
+	if (resource.statusCode >= 200 && resource.statusCode < 300) {
+		var body = JSON.parse(resource.getBody());
+		res.render('data', {resource: body});
+		return;
+	} else {
+		access_token = null;
+		if (refresh_token) {
+			// try to refresh and start again
+			refreshAccessToken(req, res);
+			return;
+		} else {
+			res.render('error', {error: 'Server returned response code: ' + resource.statusCode});
+			return;
+		}
+	}
+
 });
 
 app.get('/userinfo', function(req, res) {
-	res.end();
+
+	var headers = {
+		'Authorization': 'Bearer ' + access_token
+	};
+
+	var resource = request('GET', authServer.userInfoEndpoint,
+		{headers: headers}
+	);
+	if (resource.statusCode >= 200 && resource.statusCode < 300) {
+		var body = JSON.parse(resource.getBody());
+		console.log('Got data: ', body);
+
+		userInfo = body;
+
+		res.render('userinfo', {userInfo: userInfo, id_token: id_token});
+		return;
+	} else {
+		res.render('error', {error: 'Unable to fetch user information'});
+		return;
+	}
+
 });
 
 app.use('/', express.static('files/client'));
@@ -84,7 +251,7 @@ var buildUrl = function(base, options, hash) {
 	if (hash) {
 		newUrl.hash = hash;
 	}
-	
+
 	return url.format(newUrl);
 };
 
@@ -93,8 +260,8 @@ var encodeClientCredentials = function(clientId, clientSecret) {
 };
 
 var server = app.listen(9000, 'localhost', function () {
-  var host = server.address().address;
-  var port = server.address().port;
-  console.log('OAuth Client is listening at http://%s:%s', host, port);
+	var host = server.address().address;
+	var port = server.address().port;
+	console.log('OAuth Client is listening at http://%s:%s', host, port);
 });
- 
+
